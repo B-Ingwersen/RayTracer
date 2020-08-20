f74051054c709073b5dddd72e398b530d91d517f
B-Ingwersen <bryaningwersen@outlook.com>
2020-08-20 00:06:53 -0400

    Improve documentation/comments for scene handling functions

--- include/Scene.h
+++ include/Scene.h
@@ -3,22 +3,66 @@
 
 #include "RTTypes.h"
 
+// Scene Object Types describe how to handle a particular geometry; they must
+// be able to interpret a custom data format for that particular geometry
+// (which is passed as void * objectData). This descriptor is an interface
+// specifying how to intersect the geometry and get information about that
+// point of intersection
 struct Scene_ObjectType {
+    // a null-terminated string specifying the name of the object type
+    // eg "sphere"
     const char * name;
+
+    // 3 functions that must be implmented for each geometry in order to use
+    // them in a ray traced scene
+    
+    // try to intersect the object with a ray; if the ray does intersect, return
+    // true and record the point of intersection and the normal vector at that
+    // point through the pointers passed to the function; if there is no
+    // intersect, return false
     bool (*getIntersect)(void * objectData, RTTypesRay * ray,
         RTTypesVector * returnPoint, RTTypesVector * returnNormal);
+    
+    // try and get the intersect as described above, except that the ray passed
+    // has its origin at point of intersection on the object; the function
+    // should ensure that it does NOT return this same intersect. It may return
+    // an intersect of the same object if it is a distinctly different intersect
     bool (*getIntersectNoSelf)(void * objectData, RTTypesRay * ray,
         RTTypesVector * returnPoint, RTTypesVector * returnNormal);
+    
+    // get the color of an object at the specified point; store the following
+    // return data:
+    //  *returnColor: the Color_Float containing the color
+    //  *returnReflection: a float from 0 to 1 specifying the reflectivity of
+    //      the object at that point; 0 is completely reflective, 1 is
+    //      completely matte
     void (*getColor)(void * objectData, RTTypesVector * point,
         Color_Float * returnColor, float * returnReflection);
 };
 
+// A descriptor of all the elements of a scene, including all the objects
+// and the light source
 struct Scene_Descriptor {
-    int nObjects;
-    Scene_ObjectType * objectsType;
-    void ** objectsData;
+    int nObjects; // number of objects
+    Scene_ObjectType * objectsType; // an array of the types of each object
+    void ** objectsData; // an array of the object data
+    
+    // a point specifying the location of the light source (currently the ray
+    // tracer only supports a single, fixed format light souce)
     RTTypesVector * light;
 
+    // Get the intersection of a ray with the scene. If an intersect is found,
+    // return true and store in the pointers passed the normal vector at the
+    // intersection point, the color of the object intersected, the reflectivity
+    // of the object at that point (see Scene_ObjectType::getColor for details),
+    // and the index of the object that was intersected. If no intersection is
+    // found, return false. The closest intersect along the positive direction
+    // of the ray is returned if there are multiple intersects found
+    // The ignore index specifies the object that the ray is starting at the
+    // intersection of; if set to a valid index, the rules described in
+    // Scene_ObjectType::getIntersectNoSelf will be applied; if an invalid
+    // index is used, this behavior will be ignored. It is recommended that -1
+    // is used as an invalid index
     bool intersectRay(RTTypesRay * ray, RTTypesVector * returnPoint,
         RTTypesVector * returnNormal, Color_Float * returnColor,
         float * returnReflection, int ignoreIndex, int * returnIndex);
--- src/Scene.cpp
+++ src/Scene.cpp
@@ -2,31 +2,57 @@
 #include "Scene.h"
 #include <cfloat>
 
+// Get the intersection of a ray with the scene. If an intersect is found,
+// return true and store in the pointers passed the normal vector at the
+// intersection point, the color of the object intersected, the reflectivity
+// of the object at that point (see Scene_ObjectType::getColor for details),
+// and the index of the object that was intersected. If no intersection is
+// found, return false. The closest intersect along the positive direction
+// of the ray is returned if there are multiple intersects found
+// The ignore index specifies the object that the ray is starting at the
+// intersection of; if set to a valid index, the rules described in
+// Scene_ObjectType::getIntersectNoSelf will be applied; if an invalid
+// index is used, this behavior will be ignored. It is recommended that -1
+// is used as an invalid index
 bool Scene_Descriptor::intersectRay(RTTypesRay * ray,
     RTTypesVector * returnPoint, RTTypesVector * returnNormal,
     Color_Float * returnColor, float * returnReflection, int ignoreIndex,
     int * returnIndex) {
-    
-    int index;
 
+    // variables used for storing the information of the best intersect found
+    // so far
     *returnIndex = -1;
     RTTypesVector point;
     RTTypesVector normal;
+
+    // the distance to the closest intersect currently found
     float minDistance = FLT_MAX;
-    for (index = 0; index < nObjects; index++) {
+
+    // iterate through each object in the scene
+    for (int index = 0; index < this -> nObjects; index++) {
+
+        // if the object matches the ignore index, use the no-self intersect
+        // method; otherwise, ignore this behavior
         bool intersect;
         if (index == ignoreIndex) {
-            intersect = objectsType[index].getIntersectNoSelf(objectsData[index], ray, &point, &normal);
+            intersect = objectsType[index].getIntersectNoSelf(
+                objectsData[index], ray, &point, &normal);
         }
         else {
-            intersect = objectsType[index].getIntersect(objectsData[index], ray, &point, &normal);
+            intersect = objectsType[index].getIntersect(
+                objectsData[index], ray, &point, &normal);
         }
 
         if (intersect) {
+            // check if the intersect is in the positive direction of the ray;
+            // if it is in the wrong direction, the intersect is discarded
             RTTypesVector diff = point - ray -> point;
             if (diff.dot(ray -> direction) < 0) {
                 continue;
             }
+
+            // check if the intersect is closer than the other intersects found;
+            // if so, save it as the best intersect found
             float distance = diff.abs();
             if (distance < minDistance) {
                 minDistance = distance;
@@ -37,8 +63,11 @@ bool Scene_Descriptor::intersectRay(RTTypesRay * ray,
         }
     }
 
+    // if an intersect was found, get the color and return true; otherwise,
+    // return false
     if (*returnIndex != -1) {
-        objectsType[*returnIndex].getColor(objectsData[*returnIndex], returnPoint, returnColor, returnReflection);
+        objectsType[*returnIndex].getColor(objectsData[*returnIndex],
+            returnPoint, returnColor, returnReflection);
         return true;
     }
     return false;
