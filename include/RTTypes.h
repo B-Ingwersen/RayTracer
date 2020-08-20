c11e9bdfae5b5d0265ca07a2a9692a5b0a0d10ef
B-Ingwersen <bryaningwersen@outlook.com>
2020-08-19 23:41:40 -0400

    improve documentation/comments for RTTypes

--- include/RTTypes.h
+++ include/RTTypes.h
@@ -5,17 +5,22 @@
 #include <cstdint>
 #include <math.h>
 
+// A simple 3d vector type. Basic arithmetic and linear algebra operations are
+// implemented for it
 struct RTTypesVector {
+    // 3d coordinates
     float x;
     float y;
     float z;
 
+    // overloaded arithmetic operators
     void operator=(const RTTypesVector& rhs);
     RTTypesVector operator+(const RTTypesVector& rhs);
     RTTypesVector operator-(const RTTypesVector& rhs);
     RTTypesVector operator*(const float& rhs);
     void operator+=(const RTTypesVector& rhs);
 
+    // basic linear algebra operations
     float abs();
     float dist(const RTTypesVector v);
     float dot(const RTTypesVector v);
@@ -24,54 +29,78 @@ struct RTTypesVector {
     void invert();
 };
 
+// a packed 8-bit per channel rgba color descriptor
 struct Color {
     uint8_t r;
     uint8_t b;
     uint8_t g;
     uint8_t a;
 
+    // convert to a 32 bit integeber of the form rgb, with r in the most
+    // significant position; the alpha channel is ignored in this operation
     uint32_t u32();
+
+    // overloaded operators for basic color manipulation
     void operator+=(const Color& rhs);
     void operator/=(const int& rhs);
 };
 
+// an rgba color descriptor where each sub-color has signed 32-bit precision
 struct Color_Int {
     int r;
     int g;
     int b;
     int a;
 
+    // overloaded operators for basic color manipulation
     Color_Int operator+(const Color_Int& rhs);
     Color_Int operator*(const int& rhs);
     Color_Int operator/(const int& rhs);
     void operator+=(const Color_Int& rhs);
     void operator/=(const int& rhs);
 
+    // convert to a 32 bit integeber of the form rgb, with r in the most
+    // significant position; the alpha channel is ignored in this operation.
+    // values greater than 255 are clipped to 255
     uint32_t toUint32();
+
+    // convert from a packed color integer of the form rgb with r as the most
+    // significant bits (8 bits per color) and covert it to a Color_Int
     void fromUint32(uint32_t color);
 };
 
+// an rgba color descriptor where each component is expressed as a float
 struct Color_Float {
     float r;
     float b;
     float g;
     float a;
 
+    // overloaded operators for basic color manipulation
     void operator*=(const float& rhs);
 };
 
+// a basic structure for a ray, defined by a point of origin and a direction
 struct RTTypesRay {
+
+    // structure
     RTTypesVector point;
     RTTypesVector direction;
 
+    // create a ray from a point of origin and a point that the ray passes
+    // through
     void createFromPoints(RTTypesVector * start, RTTypesVector * through);
 };
 
+// a basic descriptor for a sphere, based on a center and radius
 struct RTTypesSphere {
     RTTypesVector center;
     float r;
 };
 
+// a basic descriptor for a plane; it is defined based on a normal vector N
+// and a value d such that the plane is described by the equation
+// N.x * x + N.y * y + N.z * z = d
 struct RTTypesPlane {
     RTTypesVector normalVector;
     float d;
--- src/RTTypes.cpp
+++ src/RTTypes.cpp
@@ -1,5 +1,6 @@
 #include "RTTypes.h"
 
+// Basic arithmetic operators for RTTypesVector
 void RTTypesVector::operator=(const RTTypesVector& rhs) {
     this -> x = rhs.x;
     this -> y = rhs.y;
@@ -34,10 +35,15 @@ void RTTypesVector::operator+=(const RTTypesVector& rhs) {
     this -> y += rhs.y;
     this -> z += rhs.z;
 }
+
+// get the absolute value of a vector
 float RTTypesVector::abs() {
     return sqrt(this -> x * this -> x + this -> y * this -> y + this -> z *
         this -> z);
 }
+
+// get the distance between two vectors (the vectors are interpreted as 3d
+// points)
 float RTTypesVector::dist(const RTTypesVector v) {
     RTTypesVector diff = {
         this -> x - v.x,
@@ -46,26 +52,39 @@ float RTTypesVector::dist(const RTTypesVector v) {
     };
     return diff.abs();
 }
+
+// get the dot product of the vector with another vector
 float RTTypesVector::dot(const RTTypesVector v) {
     return v.x * this -> x + v.y * this -> y + v.z * this -> z;
 }
+
+// return the cosine of the angle between two vectors
 float RTTypesVector::cosineOfAngleBetween(const RTTypesVector v) {
     return (this -> x * v.x + this -> y * v.y + this -> z * v.z) /
         sqrt((this -> x * this -> x + this -> y * this -> y + this -> z * 
         this -> z) * (v.x * v.x + v.y * v.y + v.z * v.z));
 }
+
+// return the angle between two vectors
 float RTTypesVector::angleBetween(const RTTypesVector v) {
     return acos( cosineOfAngleBetween(v) );
 }
+
+// negate a vector
 void RTTypesVector::invert() {
     this -> x = -this -> x;
     this -> y = -this -> y;
     this -> z = -this -> z;
 }
 
+// convert to a 32 bit integeber of the form rgb, with r in the most
+// significant position; the alpha channel is ignored in this operation
 uint32_t Color::u32() {
     return ((uint32_t)r << 16) + ((uint32_t)g << 8) + (uint32_t)b;
 }
+
+// overloaded operators for manipulating a Color; WARNING overflow is NOT
+// guarded against
 void Color::operator+=(const Color& rhs) {
     this -> r += rhs.r;
     this -> b += rhs.b;
@@ -79,7 +98,8 @@ void Color::operator/=(const int& rhs) {
     this -> a /= rhs;
 }
 
-
+// overloaded operators for manipulating a Color_Int; WARNING overflow is NOT
+// guarded against
 Color_Int Color_Int::operator+(const Color_Int& rhs) {
     Color_Int returnValue = {
         this -> r + rhs.r,
@@ -119,6 +139,10 @@ void Color_Int::operator/=(const int& rhs) {
     this -> g /= rhs;
     this -> a /= rhs;
 }
+
+// convert to a 32 bit integeber of the form rgb, with r in the most
+// significant position; the alpha channel is ignored in this operation.
+// values greater than 255 are clipped to 255
 uint32_t Color_Int::toUint32() {
     uint32_t r_ret = this -> r; if (r_ret > 255) {r_ret = 255;}
     uint32_t g_ret = this -> g; if (g_ret > 255) {g_ret = 255;}
@@ -126,12 +150,16 @@ uint32_t Color_Int::toUint32() {
 
     return (r_ret << 16) + (g_ret << 8) + (b_ret);
 }
+
+// convert from a packed color integer of the form rgb with r as the most
+// significant bits (8 bits per color) and covert it to a Color_Int
 void Color_Int::fromUint32(uint32_t color) {
-    r = (color >> 16) & 0xFF;
-    g = (color >> 8) & 0xFF;
-    b = color & 0xFF;
+    this -> r = (color >> 16) & 0xFF;
+    this -> g = (color >> 8) & 0xFF;
+    this -> b = color & 0xFF;
 }
 
+// overloaded operators for manipulating a Color_Float
 void Color_Float::operator*=(const float& rhs) {
     this -> r *= rhs;
     this -> b *= rhs;
@@ -139,6 +167,8 @@ void Color_Float::operator*=(const float& rhs) {
     this -> a *= rhs;
 }
 
+// create a ray from a point of origin and a point that the ray passes
+// through
 void RTTypesRay::createFromPoints(RTTypesVector * start,
     RTTypesVector * through) {
     
